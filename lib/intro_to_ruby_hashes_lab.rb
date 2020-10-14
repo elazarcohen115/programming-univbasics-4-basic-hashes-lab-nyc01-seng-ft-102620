c51817eb50ed52d2d60f2241ff1e4c63faf7e6f0
simon cohen <escohen115@gmail.com>
2020-10-14 22:51:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,15 @@ def my_hash
 end
 
 def pioneer
+  {
+    :name => 'Grace Hopper'
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {
+    :id => number
+  }# return a hash with a key :id assigned to the provided number
 end
